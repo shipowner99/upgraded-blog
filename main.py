50c8f149ace2d08a7fd9918cb7b0fd15e897b45b
shipowner99 <daban2@naver.com>
2023-03-14 23:54:12 +0900

    Import requests and requests blog api

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
-
+response = requests.get("https://api.npoint.io/c30ea4f6d9c5343811cb")
+data = response.json()
+print(data)
 @app.route('/')
 def get_all_posts():
     return render_template("index.html")
