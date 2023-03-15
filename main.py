9714faa03f89cd0bc9448386779f0c9ea7fedb56
shipowner99 <daban2@naver.com>
2023-03-15 17:29:07 +0900

    receive contact_input_data with flask request module

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
@@ -22,6 +22,16 @@ def contact():
 @app.route('/post/<int:num>')
 def post(num):
     return render_template("post.html", number=num, posts=data)
+
+@app.route('/submit', methods=["POST"])
+def receive_data():
+    data = request.form
+    print(data["username"])
+    print(data["email"])
+    print(data["phone"])
+    print(data["message"])
+
+    return "<h1>Successfully sent your message</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
